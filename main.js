c89625bfc97f65c4a75773523931893f1391cc1d
lale6 <lalaliyeva3216@gmail.com>
2022-11-17 13:03:44 +0400

    deyisiklik

--- main.js
+++ main.js
@@ -1,14 +1,17 @@
-// Currency buttons
+// birinci ve ikinci qutudaki butonlar
 const firstBtns = document.querySelectorAll(".i-have .currency");
 const secondBtns = document.querySelectorAll(".i-want .currency");
 
-// Input fields
+// birinci ve ikinci qutudaki text yerleri
 const firstInput = document.querySelector(".i-have input");
 const secondInput = document.querySelector(".i-want input");
 
-// Info fields
+// birinci ve ikinci qutudaki info yerleri
 const firstInfo = document.querySelector(".i-have .info");
 const secondInfo = document.querySelector(".i-want .info");
+
+// istifadeci mesaji
+const userMessage = document.querySelector(".user-message");
 
 // Requesti yazandan bir muddet sonra gondermek ucun gozletme funksiyasi
 function debounce(func, delay = 250) {
@@ -21,33 +24,7 @@ function debounce(func, delay = 250) {
   };
 }
 
-// gozleme funksiyalari
-const debouncedFromInput = debounce(() => convert("have", "want"), 800);
-const debouncedToInput = debounce(() => convert("want", "have"), 800);
-
-
-// birinci
-firstBtns.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    debouncedFromInput();
-    firstBtns.forEach((button) => button.classList.remove("active"));
-    if(!button.classList.contains("active")) {
-      button.classList.add("active");
-    }
-  });
-});
-
-secondBtns.forEach((button) => {
-  button.addEventListener("click", (e) => {
-    debouncedFromInput();
-    secondBtns.forEach((button) => button.classList.remove("active"));
-    if(!button.classList.contains("active")) { 
-      button.classList.add("active");
-    }
-  });
-});
-
-function convert(have, want) {
+function convertvalue(have, want) {
   const haveCurrency = document.querySelector(
     `.i-${have} .currency.active`
   ).id;
@@ -59,6 +36,7 @@ function convert(have, want) {
   let amount = have === "have" ? firstInput.value : secondInput.value;
   let online = navigator.onLine;
   if (online) {
+    userMessage.textContent = "";
     fetch(
       `https://api.exchangerate.host/latest?base=${haveCurrency}&symbols=${wantCurrency}`
     )
@@ -68,26 +46,65 @@ function convert(have, want) {
       .then((data) => {
         let rate = data.rates[wantCurrency];
         let total = rate * amount;
+
         if (have === "have") {
           secondInput.value = total.toPrecision(6);
         } else {
           firstInput.value = total.toPrecision(6);
         }
+
         firstInfo.textContent = `1 ${haveCurrency} = ${rate} ${wantCurrency}`;
         secondInfo.textContent = `1 ${wantCurrency} = ${(1 / rate).toPrecision(
           6
         )} ${haveCurrency}`;
       });
   } else {
-    // popupEl.classList.replace("hide", "show");
+    userMessage.textContent = "You are offline."
   }
 }
 
-firstInput.addEventListener("keypress", (e) => {
-  debouncedFromInput();
+// gozleme funksiyalari
+const delayFirstInput = debounce(() => convertvalue("have", "want"), 800);
+const delatSecondInput = debounce(() => convertvalue("want", "have"), 800);
+
+
+// birinci qutudaki valyuta butonlari
+firstBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // requesti gozlet
+    delayFirstInput();
+    // click zamani diger butonlari deaktivasiya et
+    firstBtns.forEach((button) => button.classList.remove("active"));
+    // butonu sec
+    if(!button.classList.contains("active")) {
+      button.classList.add("active");
+    }
+  });
 });
 
+// ikinci qutudaki valyuta butonlari
+secondBtns.forEach((button) => {
+  button.addEventListener("click", (e) => {
+    // requesti gozlet
+    delayFirstInput();
+    
+    // click zamani diger butonlari deaktivasiya et
+    secondBtns.forEach((button) => button.classList.remove("active"));
+    // butonu sec
+    if(!button.classList.contains("active")) { 
+      button.classList.add("active");
+    }
+  });
+});
+
+
+// delay ile request gonder
+firstInput.addEventListener("keypress", (e) => {
+  delayFirstInput();
+});
+
+// delay ile request gonder
 secondInput.addEventListener("keypress", () => {
-  debouncedToInput();
+  delatSecondInput();
 });
 
